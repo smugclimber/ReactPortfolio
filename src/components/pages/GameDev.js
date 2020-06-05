5a2022df2dcf985af8cedb478d46a5afc6a23b24
smug_climber <nhall1881@yahoo.com>
2020-06-05 02:46:39 -0400

    reduced img sizes, adj carousel

--- src/components/pages/GameDev.js
+++ src/components/pages/GameDev.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Row from "react-materialize/lib/Row";
 import proj1 from './../../images/WaveBoy_preview.jpg';
 import proj2 from './../../images/deck_gunner_title.jpg';
 import proj3 from './../../images/Spudemic_preview.jpg';
@@ -10,88 +11,104 @@ import "./../../styles/GameDev.css";
 
 const GameDev = () =>
 
-<div>
-  <div className="Row gamesBox">
-    <Col l={4} s={6} className='projCard'>
+<Row>
+  <Row className="gamesBox">
+    <Col l={4} s={12} className='projCard'>
+      
       <Card header={<CardTitle reveal image={proj1} waves='light'/>}
-    		title="Wave Boy: Virtual Reality, one delivery at a time!"
-    		reveal=
+        title="Wave Boy: Virtual papers, one delivery at a time!"
+        reveal=
         {<p>A VR experience played with an exercise bike and touch controls! Take on the role of Wave Boy, a heroic neighborhood paperboy who delivers both friendly waves and fresh copies of the Wave Street Journal to the citizens to Wave Town.<br/>
         <br/>Tech Used: Unity3D, C#, Blender, Audacity Sound Mixer
-        </p>}>
-    		<p><a href="https://globalgamejam.org/2017/games/wave-boy">GGJ WaveBoy link</a></p>
+        </p>}>          
+        <Col l={12} s={6}>
+          <a href='https://globalgamejam.org/2017/games/wave-boy'><button className='game_btn2 btn steelblue waves-effect white white-text'>GGJ Wave Boy</button></a>
+        </Col>
+        <Col l={12} s={6}>
+          <a href='https://twitter.com/smugclimber/status/823636503884537856'><button className='game_btn2 btn steelblue waves-effect white white-text'>Demo Video</button></a>
+        </Col>
       </Card>
+      <br></br>
     </Col>
-    <Col l={4} s={6} className='projCard'>
+    <Col l={4} s={12} className='projCard'>
       <Card header={<CardTitle reveal image={proj2} waves='light'/>}
-    		title="Deck Gunner: Survive the onslaught of alien ships"
-    		reveal=
+        title="Deck Gunner: Survive the onslaught of alien ships"
+        reveal=
           {<p>Deck Gunner is a modern 2D clone of 'Space Invaders'. The player, in his spaceship, is the Deck Gunner and will be able to move within the play space using the left and right arrow keys, while shooting projectiles at enemies who perpetually spawn as the player attempts to destroy them.<br/>
-          Tech Used: Unity3D, C#, Adobe Photoshop
+          <br/>Tech Used: Unity3D, C#, Adobe Photoshop
           </p>}>
-    		<p><a href="http://deckgunner.atomicnick.com">Deck Gunner Demo</a></p>
+        <a href="http://deckgunner.atomicnick.com"><button className='game_btn btn steelblue waves-effect white white-text'>Deck Gunner Demo</button></a>
       </Card>
+      <br></br>
     </Col>
-    <Col l={4} s={6} className='projCard'>
+    <Col l={4} s={12} className='projCard'>
       <Card header={<CardTitle reveal image={proj3} waves='light'/>}
-    		title="Spudemic: Survive the potato apocalypse"
-    		reveal=
+        title="Spudemic: Survive the potato apocalypse"
+        reveal=
           {<p>Meet Ida. She's tough, determined, and just a little bit pissed off. All Ida wants is to finish her shift at GamePlace and get home. But with a new flu strain, Spud Flu, on the rise, it's gonna be a tough commute. Get Ida home without letting the Spud Flu get transmitted to her.<br/>
-          Tech Used: Unity3D, C#, Adobe Illustrator
+          <br/>Tech Used: Unity3D, C#, Adobe Illustrator
           </p>}>
-    		<p><a href="https://globalgamejam.org/2018/games/spudemic">GGJ Spudemic Link</a></p>
-        <p><a href="http://spudemic.atomicnick.com">Spudemic Demo</a></p>
+          <Col l={12} s={6}>
+            <a href="https://globalgamejam.org/2018/games/spudemic"><button className='game_btn2 btn steelblue waves-effect white white-text'>GGJ Spudemic Link</button></a>
+          </Col>
+          <Col l={12} s={6}>
+            <a href="http://spudemic.atomicnick.com"><button className='game_btn2 btn steelblue waves-effect white white-text'>Spudemic Demo</button></a>
+          </Col>
       </Card>
+      <br></br>
     </Col>
-  </div>
-  <div className="projBox Row">
-    <Col l={4} s={6}className='projCard'>
+  </Row>
+  <Row>
+    <Col l={4} s={12}className='projCard'>
       <Card header={<CardTitle reveal image={proj4} waves='light'/>}
-    		title="Time Bender, repair time before it's too late!"
-    		reveal=
+        title="Time Bender, repair time!"
+        reveal=
         {<p>TimeBender is a React VR application. A game of repairing time, where you the user is anonymously contacted by a crazed scientist who has disrupted the space time continuum. Enjoy!.<br/>
-        Tech Used: ReactVR, Javascript
+        <br/>Tech Used: ReactVR, Javascript
         </p>}>
-    		<p><a href="https://time-bender.herokuapp.com/">TimeBender Demo</a></p>
+        <a href="https://time-bender.herokuapp.com/"><button className='game_btn btn steelblue waves-effect white white-text'>TimeBender Demo</button></a>
       </Card>
+      <br></br>
     </Col>
-    <Col l={4} s={6} className='projCard'>
+    <Col l={4} s={12} className='projCard'>
       <Card header={<CardTitle reveal image={proj5} waves='light'/>}
-    		title="Block Breaker: Save America!"
-    		reveal=
+        title="Block Breaker: Save America!"
+        reveal=
         {<p>Block Breaker is a clone of the all-time Atari classic 'Break-out!'. The player must travel through multiple levels breaking blocks that are taking over America! Use your trusty spike-ball to bust them out!<br/>
-        Tech Used: GIMP, Unity3D, Audacity Sound Mixer
+        <br/>Tech Used: GIMP, Unity3D, Audacity Sound Mixer
         </p>}>
-    		<p><a href="http://blockbreaker.atomicnick.com">BlockBreaker Demo</a></p>
+        <a href="http://blockbreaker.atomicnick.com"><button className='game_btn btn steelblue waves-effect white white-text'>BlockBreaker Demo</button></a>
       </Card>
+      <br></br>
     </Col>
-    <Col l={4} s={6} className='projCard'>
+    <Col l={4} s={12} className='projCard'>
       <Card header={<CardTitle reveal image={proj6} waves='light'/>}
-    		title="Retro Consoles Game"
-    		reveal=
+        title="Retro Consoles Game"
+        reveal=
         {<p>Retro Console Madness is a classic memory game that revisits the bygone days of Atari and Ninetendo classic. Players must pick images to get points, but pick the same image twice and you lose!!<br/>
-        Tech Used: React, Javascript, Audacity Sound Mixer
+        <br/>Tech Used: React, Javascript, Audacity Sound Mixer
         </p>}>
-    		<p><a href="retroconsoles.atomicnick.com">BlockBreaker Demo</a></p>
+        <a href="retroconsoles.atomicnick.com"><button className='game_btn btn steelblue waves-effect white white-text'>BlockBreaker Demo</button></a>
       </Card>
+      <br></br>
     </Col>
-  </div>
-  <div className="projBox Row">
+  </Row>
+  <Row>
     <Col l={12} s={12} className='threeDModel'>
       <h2>3D Models</h2>
     </Col>
-    <Col l={12} s={12} className='threeDModel'>
-      <div className="sketchfab-embed-wrapper">
-        <iframe className="eachModel" src="https://sketchfab.com/models/417342aa3d1546d285be6e4691ded1df/embed?autospin=1&autostart=0&camera=0&ui_infos=0&ui_controls=0&ui_stop=0"> frameBorder="0"></iframe>
-      </div>
-      <div className="sketchfab-embed-wrapper">
-        <iframe className="eachModel" src="https://sketchfab.com/models/9fba5ee278684a7ea124da2d01c00ea6/embed?autospin=1&autostart=0&camera=0&ui_infos=0&ui_controls=0&ui_stop=0" frameBorder="0"></iframe>
-      </div>
-      <div className="sketchfab-embed-wrapper">
-        <iframe className="eachModel" src="https://sketchfab.com/models/e59400d855c34be987acf55c8299e91f/embed?autospin=1&autostart=0&camera=0&ui_infos=0&ui_controls=0&ui_stop=0" frameBorder="0"></iframe>
-      </div>
+  </Row>
+  <Row className='threeDModel'>
+    <Col  l={4} s={12} className="sketchfab-embed-wrapper">
+      <iframe className="eachModel" src="https://sketchfab.com/models/417342aa3d1546d285be6e4691ded1df/embed?autospin=1&autostart=0&camera=0&ui_infos=0&ui_controls=0&ui_stop=0"> frameBorder="0"></iframe>
     </Col>
-  </div>
-</div>;
+    <Col  l={4} s={12} className="sketchfab-embed-wrapper">
+      <iframe className="eachModel" src="https://sketchfab.com/models/9fba5ee278684a7ea124da2d01c00ea6/embed?autospin=1&autostart=0&camera=0&ui_infos=0&ui_controls=0&ui_stop=0" frameBorder="0"></iframe>
+    </Col>
+    <Col  l={4} s={12} className="sketchfab-embed-wrapper">
+      <iframe className="eachModel" src="https://sketchfab.com/models/e59400d855c34be987acf55c8299e91f/embed?autospin=1&autostart=0&camera=0&ui_infos=0&ui_controls=0&ui_stop=0" frameBorder="0"></iframe>
+    </Col>
+  </Row>
+</Row>;
 
 export default GameDev;
--- src/components/pages/WebDev.js
+++ src/components/pages/WebDev.js
@@ -9,20 +9,26 @@ import proj7 from './../../images/TimeBender_preview.jpg';
 import gitLogo from "./../../images/logos/git_trans_logo.png";
 import {Carousel, Col} from 'react-materialize'
 import "./../../styles/WebDev.css";
+import Row from "react-materialize/lib/Row";
 
 const WebDev = () =>
 
   <div>
     <Carousel options={{ fullWidth: true, indicators:true}}>
-      <div className='Row workGlass'>
+      <div className='workGlass'>
         <Col s={12} l={3} className='projDescription'>
           <h6>US Army CORE CMS</h6>
           <br></br>
-      		<p className='white-text'>A Content Management System (CMS) produced for a US Army client to serve the Public Affairs Officers from any base globally. The CMS manages official news articles, photos, and other journalistic content. It provides content creation, performance metrics, a public API and organization management.<br/><br/>Tech used:<br/>PhP Laravel, SQL, Angular9, and Javascript<br/>
-          <a href="https://github.com/armydotmil"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
-          <br/><br/>
-          <a href="https://www.army.mil/core/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
+      		<p className='white-text'>A Content Management System (CMS) produced for a US Army client to serve Public Affairs Officers globally. The CMS manages news articles, photos, and videos. It provides content creation, performance metrics, a public API and organization management.<br/><br/>Tech used:<br/>PhP Laravel, SQL, Angular9, and Javascript<br/>
           </p>
+          <Row>
+            <Col s={6} l={12} className="btn_space">
+              <a href="https://github.com/armydotmil"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
+            </Col>
+            <Col s={6} l={12}>
+              <a href="https://www.army.mil/core/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
+            </Col>
+          </Row>         
         </Col>
         <Col s={12} l={9}  className='projImg'>
           <img alt="missing proj pic" src={proj1}/>
@@ -34,10 +40,15 @@ const WebDev = () =>
           <h6>Sushi Crusher</h6>
           <br></br>
       		<p className='white-text'>This is a personal project with a fun way of demonstrating an SQL database (DB) application on the cloud PaaS Heroku. Click 'Crush It!' to eat and move sushi to another part of the DB. You can also add new Sushi to the DB.<br/><br/>Tech used:<br/>Node, Express, Handlebars, MySQL, JQuery, and Javascript<br/>
-          <a href="https://github.com/smugclimber/SushiCrusher"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
-          <br/><br/>
-          <a href="https://pacific-ravine-93168.herokuapp.com/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
           </p>
+          <Row>
+            <Col s={6} l={12} className="btn_space">
+              <a href="https://github.com/smugclimber/SushiCrusher"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
+            </Col>
+            <Col s={6} l={12}>
+              <a href="https://pacific-ravine-93168.herokuapp.com/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
+            </Col>
+          </Row>         
         </Col>
         <Col s={12} l={9}  className='projImg'>
           <img alt="missing proj pic" src={proj2}/>
@@ -49,10 +60,15 @@ const WebDev = () =>
           <h6>American Heretics Film Site</h6>
           <br></br>
       		<p className='white-text'>Developed for a client with Bulter Films. The website is for an independant documentary about the state of religion in the central United States. The site has contact infomation, film trailers, press releases, and screening schedules.<br/><br/>Tech used:<br/>PhP Javascript, HTML, and CSS<br/>
-          <a href="https://github.com/smugclimber/amher"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
-          <br/><br/>
-          <a href="https://www.americanhereticsthefilm.com/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
           </p>
+          <Row>
+            <Col s={6} l={12} className="btn_space">
+              <a href="https://github.com/smugclimber/amher"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
+            </Col>
+            <Col s={6} l={12}>
+              <a href="https://www.americanhereticsthefilm.com/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
+            </Col>
+          </Row>         
         </Col>
         <Col s={12} l={9}  className='projImg'>
           <img alt="missing proj pic" src={proj3}/>
@@ -62,11 +78,17 @@ const WebDev = () =>
     	<div className='Row workGlass'>
         <Col s={12} l={3} className='projDescription'>
           <h6>BoozeClues</h6>
+          <br></br>
       		<p className='white-text'>This is a pub trivia management system. The application is capable of setting up rooms for bars to manage and patrons to join, all from your mobile device or desktop. It also features over 100K of trivia questions from its API source.<br/><br/>Tech used:<br/>Node Express, Handlebars, Socket.io, Passport User Auth, MySQL, JQuery, and Javascript<br/>
-          <a href="https://github.com/smugclimber/BoozeClues"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
-          <br/><br/>
-          <a href="https://protected-sierra-25147.herokuapp.com/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
           </p>
+          <Row>
+            <Col s={6} l={12} className="btn_space">
+              <a href="https://github.com/smugclimber/BoozeClues"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
+            </Col>
+            <Col s={6} l={12}>
+              <a href="https://protected-sierra-25147.herokuapp.com/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
+            </Col>
+          </Row>         
         </Col>
         <Col s={12} l={9} className='projImg'>
           <img alt="missing proj pic" src={proj4}/>
@@ -76,11 +98,17 @@ const WebDev = () =>
       <div className='Row workGlass'>
         <Col s={12} l={3} className='projDescription'>
           <h6>BoredBuddy</h6>
-          <p className='white-text'>BoredBuddy embraces boardgames and puzzles challenges. The project demonstrates skills in interacting with APIs from multiple sources(Amazon, Boardgamegeek, and YouTube). Users conduct boardgame database queries of a boardgame database, with the core functionality alleviating boardgame indecisiveness and providing help with a preferential and 'name-based' search engine. Additionally, if the user has a library of boardgames, they can interact with that data.<br/><br/>Tech used:<br/>Node Express, Materialize, JQuery, and Javascript<br/>
-      		<a href="https://github.com/smugclimber/BoredBuddy"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
-          <br/><br/>
-          <a href="https://calm-earth-73897.herokuapp.com/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
+          <br></br>
+          <p className='white-text'>This personal project demonstrates interacting with APIs from multiple sources(Amazon, Boardgamegeek, and YouTube). Users conduct queries of a boardgame database, with the core functionality alleviating boardgame indecisiveness and providing help with a preferential and 'name-based' search engine.<br/><br/>Tech used:<br/>Node Express, Materialize, JQuery, and Javascript<br/>
           </p>
+          <Row>
+            <Col s={6} l={12} className="btn_space">
+      		    <a href="https://github.com/smugclimber/BoredBuddy"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
+            </Col>
+            <Col s={6} l={12}>
+              <a href="https://calm-earth-73897.herokuapp.com/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
+            </Col>
+          </Row>         
         </Col>
         <Col s={12} l={9} className='projImg'>
           <img alt="missing proj pic" src={proj5}/>
@@ -90,11 +118,17 @@ const WebDev = () =>
       <div className='Row workGlass'>
         <Col s={12} l={3} className='projDescription'>
           <h6>BaggageClaim</h6>
-      		<p className='white-text'>The core features for this application are user auth through Google Firebase and travel belongings accountability. If users go on a trip or abroad, Baggage Claim can help with accountability, prevention of lost items, and providing documentation to insurance companies and authorities if items are lost or stolen.<br/><br/>Tech used:<br/>Google FireBase, VelocityJS, JQuery, and Javascript<br/>
-          <a href="https://github.com/smugclimber/BaggageClaim"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
-          <br/><br/>
-          <a href="https://smugclimber.github.io/BaggageClaim/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
+          <br></br>
+      		<p className='white-text'>The core features for this application are user auth through Google Firebase and travel belongings accountability. If users go on a trip, Baggage Claim can help with accountability, prevention of lost items, and providing documentation to insurance companies.<br/><br/>Tech used:<br/>Google FireBase, VelocityJS, JQuery, and Javascript<br/>
           </p>
+          <Row>
+            <Col s={6} l={12} className="btn_space">
+              <a href="https://github.com/smugclimber/BaggageClaim"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
+            </Col>
+            <Col s={6} l={12}>
+              <a href="https://smugclimber.github.io/BaggageClaim/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
+            </Col>
+          </Row>         
         </Col>
         <Col s={12} l={9} className='projImg'>
           <img alt="missing proj pic" src={proj6}/>
@@ -104,11 +138,17 @@ const WebDev = () =>
       <div className='Row workGlass'>
         <Col s={12} l={3} className='projDescription'>
           <h6>Time Bender</h6>
-          <p className='white-text'>TimeBender is a React VR application, with the entire experience in either Pano or Virtual Reality mode. A game of repairing time, the user is anonymously contacted by a crazed scientist who has disrupted the space time continuum. Find all the scientific parts he needs...AND HURRY!<br/><br/>Tech used:<br/>Node Express, ReactVR, ReactNative, Socket.io, and Javascript<br/>
-          <a href="https://github.com/smugclimber/time-bender"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
-          <br/><br/>
-          <a href="https://time-bender.herokuapp.com/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
+          <br></br>
+          <p className='white-text'>TimeBender is a React VR application, with the entire experience in either Pano or Virtual Reality mode. A game of repairing time, the user is anonymously contacted by a scientist who has disrupted the space time continuum. Find all the parts he needs...AND HURRY!<br/><br/>Tech used:<br/>Node Express, ReactVR, ReactNative, Socket.io, and Javascript<br/>
           </p>
+          <Row>
+            <Col s={6} l={12} className="btn_space">
+              <a href="https://github.com/smugclimber/time-bender"><button className='btn steelblue waves-effect white white-text'>Github Repo</button></a>
+            </Col>
+            <Col s={6} l={12}>
+              <a href="https://time-bender.herokuapp.com/"><button className='btn steelblue waves-effect white white-text'>Demo Site</button></a>
+            </Col>
+          </Row>         
         </Col>
         <Col s={12} l={9} className='projImg'>
           <img alt="missing proj pic" src={proj7}/>
